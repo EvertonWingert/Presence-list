aaae3d39be02586f76552e5b54b527d903713dfe
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-06-24 12:29:07 -0300

    refactor

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,36 +4,35 @@ import "./styles/style-mobile.css";
 import "./styles/style-desktop.css";
 import { CardPresence } from "../../Components/Card-presence";
 
-function studentFactory(name, present) {
-	return {
-		name: name,
-		time: new Date().toLocaleDateString("pt-br", {
-			hour: "2-digit",
-			minute: "2-digit",
-			second: "2-digit",
-		}),
-		present: present,
-	};
-}
-
 export function Home() {
 	const [studentName, setStudentName] = useState();
 	const [students, setStudents] = useState([]);
 	const [presentTotal, setPresentTotal] = useState(0);
 	const [absentTotal, setAbsentTotal] = useState(0);
 
-	function handleAddPresentStudent() {
-		const newStudent = studentFactory(studentName, true);
-
-		setStudents((prevState) => [newStudent, ...prevState]);
-		setPresentTotal(presentTotal + 1);
+	function studentFactory(name, present) {
+		return {
+			name: name,
+			time: new Date().toLocaleDateString("pt-br", {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit",
+			}),
+			present: present,
+		};
 	}
 
-	function handleAddAbsentStudent() {
-		const newStudent = studentFactory(studentName, false);
+	function handleStudent(present) {
+		console.log(present);
+		const newStudent = studentFactory(studentName, present);
 
 		setStudents((prevState) => [newStudent, ...prevState]);
-		setAbsentTotal(absentTotal + 1);
+
+		if (present) {
+			setPresentTotal(presentTotal + 1);
+		} else {
+			setAbsentTotal(absentTotal + 1);
+		}
 	}
 
 	return (
@@ -42,11 +41,11 @@ export function Home() {
 
 			<input type="text" placeholder="Nome do aluno" onChange={(e) => setStudentName(e.target.value)}></input>
 			<div className="buttons">
-				<button type="button" id="button-presence" onClick={handleAddPresentStudent}>
+				<button type="button" id="button-presence" onClick={() => handleStudent(true)}>
 					Presente
 				</button>
 
-				<button type="button" id="button-absence" onClick={handleAddAbsentStudent}>
+				<button type="button" id="button-absence" onClick={() => handleStudent(false)}>
 					Ausente
 				</button>
 			</div>
@@ -56,13 +55,8 @@ export function Home() {
 				<h3>Ausentes: {absentTotal}</h3>
 			</div>
 
-			{students.map((student) => (
-				<CardPresence
-					key={student.time}
-					user={student.name}
-					dateTime={student.time}
-					present={student.present}
-				/>
+			{students.map((student, index) => (
+				<CardPresence key={index} user={student.name} dateTime={student.time} present={student.present} />
 			))}
 		</div>
 	);
